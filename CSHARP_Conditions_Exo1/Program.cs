6751ec255a3aab8a3a751e9702582d098cc2e4df
renaudesign <renaud.sourmail@gmail.com>
2021-02-16 15:07:59 +0100

    exo1 fini

---
+++ CSHARP_Conditions_Exo1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSHARP_Conditions_Exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rainy = true;
+
+            if(rainy==true)
+            Console.WriteLine("Bonjour, attention aux pluies radioactives aujourd'hui. Pensez à mettre votre combinaison étanche intégrale Vault-Tec. Bonne journée !");
+
+            else
+            Console.WriteLine("Bonjour, aujourd'hui il n'y pas de pluies radioactives, bonne journée !");
+        }
+    }
+}
